a3a9dd99f443c2a4b97a6d608c52a19f9893af27
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-08 15:27:24 -0500

    adding the API from Coingecko

--- Components/CoinList.js
+++ Components/CoinList.js
@@ -1,3 +1,17 @@
 export default function CoinList () {
-    return <></>;
+    return (
+    <>
+    {filteredCoins.map(coin => {
+        return <Coins
+        key={coin.id}
+        name={coin.name}
+        id={coin.id}
+        price={coin.current_price}
+        symbol={coin.symbol}
+        marketcap={coin.market_cap}
+        volume={}
+        /> 
+    })}
+    </>
+    );
 }
