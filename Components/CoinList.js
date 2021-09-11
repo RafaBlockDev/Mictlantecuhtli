bc755fdcaf4de9778b21b3623eacc0dab927e9c9
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-10 22:06:02 -0500

    Expanding CoinList

--- Components/CoinList.js
+++ Components/CoinList.js
@@ -1,16 +1,21 @@
-export default function CoinList () {
+import Coins from "./Coins";
+
+export default function CoinList ({ filteredCoins }) {
     return (
     <>
     {filteredCoins.map(coin => {
-        return <Coins
+        return ( <Coins
         key={coin.id}
         name={coin.name}
         id={coin.id}
         price={coin.current_price}
         symbol={coin.symbol}
         marketcap={coin.market_cap}
-        volume={}
-        /> 
+        volume={total_volume}
+        image={coin.image}
+        priceChange={coin.price_change_percentage_24h}
+        />
+        ) 
     })}
     </>
     );
