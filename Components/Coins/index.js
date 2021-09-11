1a5d1d819052074028131875ee3b33c08a3c4006
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-10 22:42:41 -0500

    Showing all elements taked for the API

--- Components/Coins/index.js
+++ Components/Coins/index.js
@@ -9,7 +9,19 @@ const Coins = ({name, price, symbol, marketcap, volume, image, priceChange, id})
                     <h1 className="coin_h1">{name}</h1>
                     <p className="coin_symbol">{symbol}</p>
                 </div>
-
+                <div className="coin_data">
+                    <p className="coin_price">${price}</p>
+                    <p className="coin_volume">${volume.toLocaleString()}</p>
+                    {priceChange < 0 ? (
+                        <p className="coin_percent red">
+                            {priceChange.toFised(2)}%
+                        </p>
+                    ) : (
+                        <p className="coin_percent green">
+                            {priceChange.toFixed(2)}%
+                        </p>
+                    )}
+                </div>
             </div>
 
         </div>
