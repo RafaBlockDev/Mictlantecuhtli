4c473e1262b8c8bb3b57e8393ade24dc5064e0ac
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-10 22:32:58 -0500

    Adding and showing the CoinList to the front

--- Components/Coins/index.js
+++ Components/Coins/index.js
@@ -1,10 +1,19 @@
 import React from 'react'
 
-const Coins = ({name}) => {
+const Coins = ({name, price, symbol, marketcap, volume, image, priceChange, id}) => {
     return (
-        <div>
-            <h1>{name}</h1>
+        <div className="coin_container">
+            <div className="coin_row">
+                <div className="coin">
+                    <img src={image} alt={name} className="coin_img"/>
+                    <h1 className="coin_h1">{name}</h1>
+                    <p className="coin_symbol">{symbol}</p>
+                </div>
+
+            </div>
+
         </div>
+
     )
 }
 
