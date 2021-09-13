b8937664108d7a80d20cd2cdca696dadabe801e1
RafaCypherpunk <rafafuentesrangel@outlook.com>
2021-09-12 20:37:17 -0500

    Layout

---
+++ Components/Layout.js
@@ -0,0 +1,8 @@
+const Layout = ({children, title = "Crypto Tracker"}) => {
+    <div className="layout">
+        <Head>
+            <title>{title}</title>
+            <link rel="icon" href="./favicon.png" />
+        </Head>
+    </div>
+}
